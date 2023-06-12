810885a271c687fc0aa455f0261129314db23ecc
emilioff8822 <emilioff@hotmail.it>
2023-06-12 18:04:54 +0200

    commit-bonus-seeder

--- dc-comics/resources/views/partials/footer.blade.php
+++ dc-comics/resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
-<footer class="container">
+<footer class="container text-center">
 
 
-    <h1>footer</h1>
+    <h1 class=""">footer</h1>
+    <img class="" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
 
 </footer>
--- dc-comics/resources/views/partials/header.blade.php
+++ dc-comics/resources/views/partials/header.blade.php
@@ -1,9 +1,11 @@
-<header>
+<header class="text-center">
     <div class="container">
+        <img class="my-5" src="/img/dc-logo.png" alt="">
+
 
 
         <nav class="nav">
-            <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Index</a>
+            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
             <a class="nav-link" href="{{ route('contatti') }}">Contatti</a>
             <a class="nav-link" href="#">Comics</a>
         </nav>
