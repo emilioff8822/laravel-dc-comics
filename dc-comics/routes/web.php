e5c54b50c5aee572b25588eb34860607969ce2fb
emilioff8822 <emilioff@hotmail.it>
2023-06-12 18:58:01 +0200

    commit-resource-route

--- dc-comics/routes/web.php
+++ dc-comics/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class ,'index'])->name('home');
 Route::get('/contatti', [PageController::class ,'contatti'])->name('contatti');
+
+Route::resource('comics', ComicController::class);
