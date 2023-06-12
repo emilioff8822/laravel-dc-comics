95441fb7979d00458e723539aa0049a54f7652f0
emilioff8822 <emilioff@hotmail.it>
2023-06-12 15:58:48 +0200

    commit-layout

---
+++ dc-comics/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('content')
+    <div>
+        <h1>home</h1>
+    </div>
+@endsection
---
+++ dc-comics/resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="container">
+
+
+    <h1>footer</h1>
+
+</footer>
