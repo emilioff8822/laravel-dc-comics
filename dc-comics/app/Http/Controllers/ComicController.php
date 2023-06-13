d9e37985698d996fef81e35396c9936e2f6c7ec2
emilioff8822 <emilioff@hotmail.it>
2023-06-13 17:21:48 +0200

    commit-errors

--- dc-comics/app/Http/Controllers/ComicController.php
+++ dc-comics/app/Http/Controllers/ComicController.php
@@ -40,20 +40,30 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {  //PRIMA DI TUTTO VALIDO I DATI, REQUEST VALIDATE VEDE SE SONO VALIDI ALTRIMENTI REINDIRIZZA AL FORM
-        $request->validate([
-        'title'=> 'required|min:3',
-        'description'=> 'required'
-        ],
-        ['title.required'=>'il titolo è un campo obbligatorio ',
-        'title.min'=> 'Il titolo deve avere almeno :min caratteri',
-        'description.required'=> 'La descrizione è un campo obbligatorio'
-
-
-
-        ]
-
-
-    );
+    $request->validate([
+    'title' => 'required|min:3',
+    'description' => 'required',
+    'thumb' => 'required',
+    'price' => 'required|numeric',
+    'series' => 'required',
+    'sale_date' => 'required|date',
+    'type' => 'required',
+    'artists' => 'required',
+    'writers' => 'required',
+], [
+    'title.required' => 'Il titolo è un campo obbligatorio.',
+    'title.min' => 'Il titolo deve avere almeno :min caratteri.',
+    'description.required' => 'La descrizione è un campo obbligatorio.',
+    'thumb.required' => 'Il thumb è un campo obbligatorio.',
+    'price.required' => 'Il prezzo è un campo obbligatorio.',
+    'price.numeric' => 'Il prezzo deve essere un numero.',
+    'series.required' => 'La serie è un campo obbligatorio.',
+    'sale_date.required' => 'La data di vendita è un campo obbligatorio.',
+    'sale_date.date' => 'La data di vendita deve essere una data valida.',
+    'type.required' => 'Il tipo è un campo obbligatorio.',
+    'artists.required' => 'Gli artisti sono un campo obbligatorio.',
+    'writers.required' => 'Gli scrittori sono un campo obbligatorio.',
+]);
 
 
 
